f00fc05969d38910857859ac70dc9ba262207f67
Agustin Carrasco <asermax@gmail.com>
2019-08-15 12:58:49 -0300

    feat(exceptions): add error code to the custo exception class

--- zoomus/exceptions.py
+++ zoomus/exceptions.py
@@ -6,6 +6,7 @@ __all__ = ['ZoomException']
 class ZoomException(requests.RequestException):
     def __init__(self, response):
         error_details = response.json()
+        self.error_code = error_details['code']
         message = error_details['message']
 
         super().__init__(message, response=response)
